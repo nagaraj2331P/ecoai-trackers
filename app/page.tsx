00eef4ab4ab26c18ae22e5dcb8fe9986a32096bd
nagaraj2331P <manivasgam15@gmail.com>
2025-06-16 16:32:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import CarbonCalculator from '@/components/CarbonCalculator';
 export default function Home() {
   return (
 
-    <main className="p-8">
-      <h1 className="text-5xl text-center font-bold">🌱EcoAI Sustainability Tracker</h1>
+    <main className="p-0">
+      <h1 className="text-5xl text-center font-bold w-full h-20 bg-pink-700 flex items-center justify-center relative shadow-md">🌱EcoAI Sustainability Tracker</h1>
       <p className="text-2xl text-center">Welcome to the future of smart environmental tracking.</p>
       <p className="p-8"></p>
       <AskAI/>
